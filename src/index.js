6e79979e1c317e84e2754e83c5706d3fb98f6b27
HeskeyBaozi <hezhiyu233@foxmail.com>
2016-10-21 01:09:49 +0800

    add picture

--- src/index.js
+++ src/index.js
@@ -12,29 +12,34 @@ import {
     search
 } from  './helper.js'
 import Node from './node.js';
-
+import {Type, Img} from './type.js';
 import {selectElement, selectNumber} from './selector.js';
 
 const model = new Model({
     target: '.container',
     data: {
         imgUrl: '',
-        blockMap: Array.from(document.querySelectorAll('.block'))
-            .map((element, index) => ({
-                element,
-                correctIndex: index
-            })),
-        blankBlock: document.querySelector('#blk-15'),
+        blockMap: Array
+            .from(document.querySelectorAll('.block'))
+            .map((element, index) => ({element, correctIndex: index})),
         startButton: 'Start!',
-        isReset: true,
-        isBaffling: false
+        gameState: Type.Unstarted,
+        isRunning: false,
+        time: 0,
+        count: 0
     }
 });
 
+const util = {
+    blankElement: document.querySelector('#blk-15'),
+    loadingElement: document.querySelector('#loading'),
+    btnElement: document.querySelector('#start'),
+    selectorsElement: document.querySelector('.img-groups'),
+    updater: update.bind(model)
+};
+
 
 model.$watch('imgUrl', function (newValue, oldValue) {
-    if (!this.isReset)
-        return;
     const img = new Image();
     const ctxArray = this.blockMap.map(object => object.element.getContext('2d'));
     img.src = newValue;
@@ -48,9 +53,32 @@ model.$watch('imgUrl', function (newValue, oldValue) {
         });
 
         const last = ctxArray[ctxArray.length - 1];
-        last.fillStyle = 'rgba(120, 120, 120, 0.3)';
+        last.fillStyle = 'rgba(120, 120, 120, 0.7)';
         last.fillRect(0, 0, 100, 100);
     };
+});
+
+model.$watch('gameState', function (newValue, oldValue) {
+    switch (newValue) {
+        case Type.Pending:
+            util.selectorsElement.classList.add('no-see');
+            break;
+    }
+
+    switch (oldValue) {
+        case Type.Pending:
+            util.selectorsElement.classList.remove('no-see');
+            break;
+    }
+});
+
+model.$watch('isRunning', function (isRunning) {
+    if (isRunning) {
+        util.btnElement.classList.add('disable-click');
+        util.selectorsElement.classList.add('no-see');
+    } else {
+        util.btnElement.classList.remove('disable-click');
+    }
 });
 
 model.imgUrl = './img/panda.jpg';
@@ -58,82 +86,82 @@ model.imgUrl = './img/panda.jpg';
 Query('#start')
     .on('click', e => {
         e.preventDefault();
-        if (model.isBaffling)
+        if (model.isRunning) {
             return;
-        const getIndex = getterFactory(model.blockMap, selectElement);
-        const [
-            updater,
-            blankTargetIndex
-        ] = [
-            update.bind(model),
-            getIndex(model.blankBlock)
-        ];
-        const targetIndex = getSample(getNeighboursIndex(blankTargetIndex));
-        new Promise((resolve, reject) => {
-            model.isBaffling = true;
-            e.target.classList.add('disable-click');
-            randomlySelectAndMoveAsync(25, 120, resolve)(targetIndex, blankTargetIndex, model.blockMap, updater);
-        }).then(node => {
-            model.isBaffling = false;
-            e.target.classList.remove('disable-click');
-        });
+        }
+        switch (model.gameState) {
 
+            case Type.Unstarted:
+
+                model.startButton = 'Mixing...';
+                const getElementIndex = getterFactory(model.blockMap, selectElement);
+                const targetIndex = [
+                    util.blankElement,
+                    getElementIndex,
+                    getNeighboursIndex,
+                    getSample
+                ].reduce((value, wrapper) => wrapper(value));
+
+                new Promise((resolve, reject) => {
+                    model.isRunning = true;
+                    randomlySelectAndMoveAsync(20, 120, resolve)(
+                        targetIndex, getElementIndex(util.blankElement), model.blockMap, util.updater
+                    );
+                }).then(node => {
+                    model.isRunning = false;
+                    model.startButton = 'Admit Defeat! : (';
+                    model.gameState = Type.Pending;
+                });
+                break;
+
+            case Type.Pending:
+                new Promise((resolve, reject) => {
+                    model.startButton = 'Getting Solution...';
+                    util.loadingElement.classList.remove('disable-see');
+                    const getIndex = getterFactory(model.blockMap, selectElement);
+                    setTimeout(() => {
+                        resolve(AStar(new Node(model.blockMap, getIndex(util.blankElement), null)));
+                    }, 20);
+                }).then(result => {
+
+                    const path = [];
+                    let target = result;
+                    while (target.parentNode) {
+                        path.push(target);
+                        target = target.parentNode;
+                    }
+
+                    return new Promise((resolve, reject) => {
+                        model.isRunning = true;
+                        util.loadingElement.classList.add('disable-see');
+                        model.startButton = 'Recovering...';
+                        showSolutionAsync(path, util.updater, resolve);
+                    });
+                }).then(path => {
+                    model.startButton = 'Try Again! : )';
+                    model.isRunning = false;
+                    model.gameState = Type.Unstarted;
+                });
+                break;
+        }
     });
-
 
 Query('.playground')
     .on('click', e => {
         e.preventDefault();
         const getIndex = getterFactory(model.blockMap, selectElement);
-        const nextDescriptor = move(getIndex(e.target), getIndex(model.blankBlock), model.blockMap);
+        const nextDescriptor = move(getIndex(e.target), getIndex(util.blankElement), model.blockMap);
         if (!nextDescriptor) return;
         update.call(model, nextDescriptor.state);
     });
 
-Query('#solution')
-    .on('click', e => {
-        e.preventDefault();
-        const loadingElement = document.querySelector('#loading');
-
-        const updater = update.bind(model);
-        const initialState = model.blockMap;
-        const getIndex = getterFactory(model.blockMap, selectElement);
-        const blankTargetIndex = getIndex(model.blankBlock);
-        new Promise((resolve, reject) => {
-            loadingElement.classList.remove('disable-see');
-            resolve();
-        }).then(() => {
-            return new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    resolve(AStar(new Node(initialState, blankTargetIndex, null)));
-                }, 20);
-            });
-        }).then(result => {
-
-            const path = [];
-            let target = result;
-            while (target.parentNode) {
-                path.push(target);
-                target = target.parentNode;
-            }
-
-            return new Promise((resolve, reject) => {
-                e.target.classList.add('disable-click');
-                loadingElement.classList.add('disable-see');
-                showSolutionAsync(path, updater, resolve);
-            });
-        }).then(path => {
-            e.target.classList.remove('disable-click');
-        });
-    })
-;
 
 function showSolutionAsync(path, updater, resolve) {
     if (path.length) {
         updater(path.pop().state);
         setTimeout(() => {
             showSolutionAsync(path, updater, resolve);
-        }, 200);
+        }, 120);
     } else {
         resolve(path);
     }
@@ -194,5 +222,13 @@ function IDAStar(rootNode) {
 
 }
 
+Query('.img-groups')
+    .on('click', e => {
+        e.preventDefault();
+        if (model.gameState === Type.Unstarted) {
+            const currentImg = Img.find(imageInfo => imageInfo.name === e.target.id);
+            model.imgUrl = currentImg.url.img;
+        }
+    });
 
 console.log(model);
